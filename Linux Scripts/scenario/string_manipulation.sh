c970c9fda177ea4373cad398b00f5cab0becf5aa
Amutheswaran <amutheswaran@outlook.com>
2023-08-10 20:45:47 +0530

    string play

---
+++ Linux Scripts/scenario/string_manipulation.sh
@@ -0,0 +1,18 @@
+NAME="Amu"
+echo $NAME
+echo ${NAME}
+echo "$NAME"
+echo "Hello $NAME"
+
+echo "Length: ${#NAME}"
+
+str1="How"
+str2="are"
+str3="you?"
+
+echo $str1$str2$str3
+# echo $str1 $str2 $str3
+strfull="$str1 $str2 $str3"
+echo $strfull
+
+
